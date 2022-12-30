5906d2886cdc0bdaa9984aa0b199e1bb3735a312
Betovn02 <97416520+Betovn02@users.noreply.github.com>
2022-12-29 22:22:04 -0600

    update

--- index.js
+++ index.js
@@ -119,5 +119,5 @@ app.get('/account/all', function (req, res) {
 });
 
 let port = process.env.PORT || 3000;
-app.listen(port);
+app.listen(process.env.PORT || 3000);
 console.log('Running on port: ' + port);
