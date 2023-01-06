19d2cb736d96fba6e70c03d170c83642d4c26731
Betovn02 <97416520+Betovn02@users.noreply.github.com>
2023-01-05 20:04:41 -0600

    update

--- admin.js
+++ admin.js
@@ -20,4 +20,24 @@ admin.initializeApp({
     }),
 });
 
+const fs = require('fs');
+
+let config = require('./firebase.json')
+
+let data = Buffer.from(JSON.stringify(config)).toString('base64')
+
+fs.writeFile("googlecred.txt", data, (err) => {
+
+if (err) console.log(err);
+
+else { console.log("File written successfully\n"); }});
+
+
+
+//After, you'll need to convert that base64 back into the credentials....
+
+const googleServiceAccountCreds = JSON.parse(Buffer.from(process.env.GOOGLE_CONFIG, 'base64').toString('ascii'))
+
+if (!googleServiceAccountCreds) throw new Error('The $GOOGLE_SERVICE_ACCOUNT_CREDS environment variable was not found!');
+
 module.exports = admin;
